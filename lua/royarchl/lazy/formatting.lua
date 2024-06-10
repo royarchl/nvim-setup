dda930f92e3810073e9c7d4f205f2a7ea30239e2
royarchl <royl.developer.2019@gmail.com>
2024-06-09 20:37:14 -0700

    Sync local and remote repository

---
+++ lua/royarchl/lazy/formatting.lua
@@ -0,0 +1,18 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
+
+        conform.setup({
+            formatters_by_ft = {
+                cpp = { "clang-format" },
+            },
+            format_on_save = {
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 500,
+            },
+        })
+    end,
+}
