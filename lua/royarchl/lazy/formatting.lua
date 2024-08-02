36355b36a13ce372d00fbde352e4ae020f496868
royarchl <royl.developer.2019@gmail.com>
2024-08-02 14:20:53 -0700

    Update syntax

--- lua/royarchl/lazy/formatting.lua
+++ lua/royarchl/lazy/formatting.lua
@@ -1,18 +1,19 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local conform = require("conform")
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
 
-        conform.setup({
-            formatters_by_ft = {
-                cpp = { "clang-format" },
-            },
-            format_on_save = {
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500,
-            },
-        })
-    end,
+    conform.setup({
+      formatters_by_ft = {
+        cpp = { "clang-format" },
+      },
+      format_on_save = {
+        -- lsp_fallback = true,
+        lsp_format = "fallback",
+        async = false,
+        timeout_ms = 500,
+      },
+    })
+  end,
 }
--- lua/royarchl/lazy_init.lua
+++ lua/royarchl/lazy_init.lua
@@ -12,6 +12,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = "royarchl.lazy",
-    change_detection = { notify = false }
+  spec = "royarchl.lazy",
+  change_detection = { notify = false }
 })
